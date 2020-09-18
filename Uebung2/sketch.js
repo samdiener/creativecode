b801601e0d8337be1d21d0b287a0c272719306cd
Sam Diener <samdiener@users.noreply.github.com>
2020-09-18 22:16:04 +0200

    Add files via upload

---
+++ Uebung2/sketch.js
@@ -0,0 +1,141 @@
+function setup() {
+  createCanvas(600, 400);
+
+  noStroke();
+}
+
+function draw() {
+  background(0);
+
+  let a=10;
+
+  fill(255,255,255,5);
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+    a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+    a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+    a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+  a+=10;
+  ellipse(width/2,height/2,a,a);
+
+
+
+
+}
